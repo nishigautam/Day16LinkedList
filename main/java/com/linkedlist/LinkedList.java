7f135464f0acfd450c16fff087b476cf5da524d8
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-05-03 19:29:32 +0530

    Added: Ability to add linked list

--- main/java/com/linkedlist/LinkedList.java
+++ main/java/com/linkedlist/LinkedList.java
@@ -23,6 +23,21 @@ public class LinkedList<T> {
         }
     }
 
+    /**
+     * uc2 : ability to create linked list by adding 30 and 56 to 70
+     */
+    public void add(T data) {
+        Node<T> newNode = new Node<T>(data);
+        if (head == null && tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.setNext(head);
+            head = newNode;
+        }
+    }
+
+
     public void display() {
         Node<T> node = head;
         while (node != null) {
--- main/java/com/linkedlist/Main.java
+++ main/java/com/linkedlist/Main.java
@@ -3,10 +3,16 @@ package com.linkedlist;
 public class Main {
     public static <T> void main(String[] args) {
         LinkedList<Integer> linkedList = new LinkedList<Integer>();
-        Integer a = 56, b = 30, c = 70;
-        linkedList.insert(a);
-        linkedList.insert(b);
-        linkedList.insert(c);
+        Integer x = 56, y = 30 , z = 70, a = 70, b = 30, c = 56;
+
+        linkedList.insert(x);
+        linkedList.insert(y);
+        linkedList.insert(z);
+
+        linkedList.add(a);
+        linkedList.add(b);
+        linkedList.add(c);
+
         linkedList.display();
     }
 }
